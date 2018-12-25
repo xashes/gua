279f2ce90133e7238d72371c84494690752e2a7f
Keller <kellerx0319@gmail.com>
2018-12-25 22:19:01 +0800

    generate gua

--- jinqiangua.rkt
+++ jinqiangua.rkt
@@ -7,14 +7,23 @@
           [jinqiangua (-> (listof zero-or-one/c))]))
 
 (define (jinqiangua)
-  '()
+  (for/list ([i (in-range 6)])
+    (generate-yao))
   )
 
 (define (generate-yao)
   (let ([zero-count (count zero? (random-gua 3))])
     (case zero-count
       [(0) 9]
-      [(1) 0]
-      [(2) 1]
+      [(1) 8]
+      [(2) 7]
       [(3) 6]
       )))
+
+(jinqiangua)
+
+(struct gua (name code pos) #:transparent)
+(define dt
+  (list (gua "乾" '(1 1 1) "south")
+        (gua "坤" '(0 0 0) "north"))
+  )
