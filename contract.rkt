5693e17b4e8e3c4726d7c148300f4f14df85dc96
xashes <xashes@outlook.com>
2019-04-23 11:30:14 +0800

    refactor

--- contract.rkt
+++ contract.rkt
@@ -1,12 +1,12 @@
 #lang racket
 
 (provide (contract-out
-          [zero-or-one/c (-> number? boolean?)]
+          [yao/c (-> number? boolean?)]
           [3-or-6/c (-> number? boolean?)]
           ))
 
-(define (zero-or-one/c n)
-  (or (zero? n) (= n 1)))
+(define (yao/c n)
+  (or/c (zero? n) (= n 1)))
 
 (define (3-or-6/c n)
   (or (= n 3) (= n 6)))
