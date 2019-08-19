be5b22d55f19cf8ab876d2878f6bab0def9ca92d
xashes <kellerx0319@gmail.com>
2019-08-19 22:30:15 +0800

    use chinese

--- data.rkt
+++ data.rkt
@@ -1,47 +1,48 @@
 #lang racket
 
-(require gua/contract)
+(require "gua.rkt"
+         json)
 
 (provide (contract-out
-          [gua (hash/c (listof zero-or-one/c) hash?)]))
+          [gua8-data (hash/c gua8? hash?)]))
 
-(define gua
+(define gua8-data
   (hash
-   '(1 1 1) (hash 'name '乾 'xiang '天 'pre-num 1 'post-num 6 'pre-pos 'south 'post-pos 'north-west)
-   '(0 1 0) (hash 'name '坎 'xiang '水 'pre-num 6 'post-num 1 'pre-pos 'west 'post-pos 'north)
-   '(0 0 1) (hash 'name '艮 'xiang '山 'pre-num 7 'post-num 8 'pre-pos 'north-west 'post-pos 'north-east)
-   '(1 0 0) (hash 'name '震 'xiang '雷 'pre-num 4 'post-num 3 'pre-pos 'north-east 'post-pos 'east)
-   '(0 1 1) (hash 'name '巽 'xiang '风 'pre-num 5 'post-num 4 'pre-pos 'south-west 'post-pos 'south-east)
-   '(1 0 1) (hash 'name '离 'xiang '火 'pre-num 3 'post-num 9 'pre-pos 'east 'post-pos 'south)
-   '(0 0 0) (hash 'name '坤 'xiang '地 'pre-num 8 'post-num 2 'pre-pos 'north 'post-pos 'south-west)
-   '(1 1 0) (hash 'name '兑 'xiang '泽 'pre-num 2 'post-num 7 'pre-pos 'south-east 'post-pos 'west)
+   '(1 1 1) (hash 'name '乾 'xiang '天 'pre-num 1 'post-num 6 'pre-pos '南 'post-pos '西北)
+   '(0 1 0) (hash 'name '坎 'xiang '水 'pre-num 6 'post-num 1 'pre-pos '西 'post-pos '北)
+   '(0 0 1) (hash 'name '艮 'xiang '山 'pre-num 7 'post-num 8 'pre-pos '西北 'post-pos '东北)
+   '(1 0 0) (hash 'name '震 'xiang '雷 'pre-num 4 'post-num 3 'pre-pos '东北 'post-pos '东)
+   '(0 1 1) (hash 'name '巽 'xiang '风 'pre-num 5 'post-num 4 'pre-pos '西南 'post-pos '东南)
+   '(1 0 1) (hash 'name '离 'xiang '火 'pre-num 3 'post-num 9 'pre-pos '东 'post-pos '南)
+   '(0 0 0) (hash 'name '坤 'xiang '地 'pre-num 8 'post-num 2 'pre-pos '北 'post-pos '西南)
+   '(1 1 0) (hash 'name '兑 'xiang '泽 'pre-num 2 'post-num 7 'pre-pos '东南 'post-pos '西)
    ))
 
 (define xing
   (hash
-   'jin (hash 'name '金
-              'sheng 'shui
-              'ke 'mu
-              'pos 'west
+   '金 (hash 'name '金
+              'sheng '水
+              'ke '木
+              'pos '西
               'tiangan '(庚 辛))
-   'mu (hash 'name '木
-              'sheng 'huo
-              'ke 'tu
-              'pos 'east
+   '木 (hash 'name '木
+              'sheng '火
+              'ke '土
+              'pos '东
               'tiangan '(甲 乙))
-   'shui (hash 'name '水
-              'sheng 'mu
-              'ke 'huo
-              'pos 'north
+   '水 (hash 'name '水
+              'sheng '木
+              'ke '火
+              'pos '北
               'tiangan '(壬 癸))
-   'huo (hash 'name '火
-              'sheng 'tu
-              'ke 'jin
-              'pos 'south
+   '火 (hash 'name '火
+              'sheng '土
+              'ke '金
+              'pos '南
               'tiangan '(丙 丁))
-   'tu (hash 'name '土
-              'sheng 'jin
-              'ke 'shui
+   '土 (hash 'name '土
+              'sheng '金
+              'ke '水
               'pos 'center
               'tiangan '(戊 己))
    ))
